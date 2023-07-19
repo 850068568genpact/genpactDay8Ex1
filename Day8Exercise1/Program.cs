ec0ad1eb297013eb9d9a723114146d3049c7a738
850068568 genpact <850068568@genpact.com>
2023-07-19 11:52:34 +0530

    INiTIaL cOmmit

---
+++ Day8Exercise1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Excercise1
+{
+    public delegate void SpinVal(int a, int b);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SpinClass obj = new SpinClass(); 
+
+            int energy = 0; 
+            int winprob = 0;
+
+            SpinVal spinvalue = new SpinVal(obj.TenthSpin);
+            spinvalue -= obj.TenthSpin;
+
+            string name; 
+            int LuckyNum;
+            Console.WriteLine("Enter you Name :");
+            name = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter Lucky Number between 1-10 :");
+                LuckyNum = int.Parse(Console.ReadLine());
+
+                switch (LuckyNum)
+                {
+                    case 1: 
+                        spinvalue += obj.FirstSpin; 
+                        break;
+                    case 2:
+                        spinvalue += obj.SecondSpin; 
+                        break;
+                    case 3:
+                        spinvalue += obj.ThirdSpin; 
+                        break;
+                    case 4:
+                        spinvalue += obj.FourthSpin; 
+                        break;
+                    case 5:
+                        spinvalue += obj.FifthSpin; 
+                        break;
+                    case 6:
+                        spinvalue += obj.SixthSpin; 
+                        break;
+                    case 7:
+                        spinvalue += obj.SeventhSpin; 
+                        break;
+                    case 8:
+                        spinvalue += obj.EighthSpin; 
+                        break;
+                    case 9:
+                        spinvalue += obj.NinthSpin; 
+                        break;
+                    case 10:
+                        spinvalue += obj.TenthSpin; 
+                        break;
+                    default: Console.WriteLine("Invalid Lucky Number!!!"); 
+                        break;
+                }
+            }
+            spinvalue(energy, winprob);
+
+            if (energy >= 4 && winprob > 60)
+            {
+                Console.WriteLine("You're a Winner!!!");
+            }
+            else
+            {
+                Console.WriteLine("You've Lost!!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day8Exercise1/SpinClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Excercise1
+{
+    public class SpinClass
+    {
+        public void FirstSpin(int energy, int winprobability)
+            {
+                energy += 1; winprobability += 10;
+            }
+        public void SecondSpin(int energy, int winprobability)
+            {
+                energy += 2; winprobability += 20;
+            }
+        public void ThirdSpin(int energy, int winprobability)
+            {
+                energy -= 3; winprobability -= 30;
+            }
+        public void FourthSpin(int energy, int winprobability)
+            {
+                energy += 4; winprobability += 40;
+            }
+        public void FifthSpin(int energy, int winprobability)
+            {
+                energy -= 5; winprobability -= 50;
+            }
+        public void SixthSpin(int energy, int winprobability)
+            {
+                energy -= 1; winprobability -= 60;
+            }
+        public void SeventhSpin(int energy, int winprobability)
+            {
+                energy += 1; winprobability += 70;
+            }
+        public void EighthSpin(int energy, int winprobability)
+            {
+                energy -= 2; winprobability -= 20;
+            }
+        public void NinthSpin(int energy, int winprobability)
+            {
+                energy -= 3; winprobability -= 30;
+            }
+        public void TenthSpin(int energy, int winprobability)
+            {
+                energy += 10; winprobability += 100;
+            }
+    }
+}
